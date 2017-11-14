9de6b46f0a697c0968ca5f771b4bda1854d53329
Teo Freund <tfreund95@gmail.com>
2017-11-14 09:38:50 -0300

    Agregadas un par de cosas

--- TW.py
+++ TW.py
@@ -2,33 +2,61 @@ from googletrans import Translator
 import json
 from pprint import pprint
 import networkx as nx
+import matplotlib.pyplot as plt
 
-def rewrite(lista):
+def rewrite(lista, G, idiomas=['en', 'es']):
+    idx = 0
     t = Translator()
-    actual = 'en'
-    prox = 'es'
+
     while True:
-        trad = t.translate(lista[-1], src = actual, dest = prox).text
+        next_idx = (idx + 1) % len(idiomas)
+
+        actual = idiomas[idx]
+        prox = idiomas[next_idx]
+        trad = t.translate(lista[-1], src = actual, dest = prox).text.lower()
         if trad in lista:
+            G.add_edge(lista[-1], trad)
             break
+        G.add_edge(lista[-1], trad)
         lista.append(trad)
-        temp = actual
-        actual = prox
-        prox = temp
+        idx = next_idx
+
     res = ""
     for t in lista:
         res += " -> {}".format(t)
-    print (res)
-    return lista
+    #print (res)
+    return G
 
-#inicial = str(input())
 
-#print (rewrite([inicial]))
+def drawConnected(G, save=False, file="temp.png"):
+    largest_cc = nx.DiGraph()
+    value = (0, 0)
+    for candidate in nx.weakly_connected_component_subgraphs(G, copy=True):
+        temp = 1
+        for node in candidate.nodes():
+            if candidate.in_degree(node) > 0 :
+                temp = temp*candidate.in_degree(node)
+        if (temp, len(candidate)) > value:
+            value = (temp, len(candidate))
+            largest_cc = candidate
+
+    plt.clf()
+    fig = plt.figure(figsize=(15,15))
+    nx.draw_kamada_kawai(largest_cc, with_labels=True, node_size=0, edge_color='#efefef')
+    if save:
+        fig.savefig(file)
+    else:
+        fig.show()
+    plt.close(fig)
+
+H = nx.DiGraph()
 
 with open('words_dictionary.json') as data_file:
     data = json.load(data_file)
 
+cnt = 0
 for k in data.keys():
-    rewrite([k])
-    #data[k] = t.translate(k, src='en', dest='es').text
-    #print ("{} -> {}".format(k, data[k]))
+    print ("cnt: {}             ".format(cnt), end='\r')
+    cnt +=1
+    H = rewrite([k], H, ['en', 'es'])
+    drawConnected(H, save=True)
